5cb3ac49211f567ac25c8c372cd95d1b2f1f53e2
Learner <64130518+xzinc@users.noreply.github.com>
2022-02-13 12:42:13 +0530

    Update stream.py

--- Megatron/bot/plugins/stream.py
+++ Megatron/bot/plugins/stream.py
@@ -80,7 +80,7 @@ async def media_receive_handler(c: Client, m: Message):
         stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
         short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
         logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
-        msg_text = f"Your Link Generated! 😄\n\nلینک پر سرعت شما ایجاد شد! 😄\n\n📂 **File Name:** `{file_name}`\n\n**✨ File Size:** `{file_size}`\n\n📥 **Direct/Stream Link:** `{stream_link}`\n\n📥 **Short Link:** `{short_link}`"
+        msg_text = f"Your Link Generated!😄\n\n📂 **File Name:** `{file_name}`\n\n**✨ File Size:** `{file_size}`\n\n📥 **Download/Stream Link:** `{stream_link}`\n\n📥 **Short Link:** `{short_link}`"
         await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Requested by [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID:** `{m.from_user.id}`\n**Download Link:** {stream_link}\n**Short Link:** {short_link}", disable_web_page_preview=True, reply_to_message_id=log_msg.message_id, parse_mode="Markdown", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("࿋ Ban User ࿋", callback_data=f"ban_{m.from_user.id}")]])) 
         await m.reply_text(
             text=msg_text, 
