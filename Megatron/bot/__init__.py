b1bc7fa1918bc0da6d9b2c20971e0d5b80b7152b
Learner <64130518+xzinc@users.noreply.github.com>
2023-12-06 09:42:36 +0530

    Update __init__.py

--- Megatron/bot/__init__.py
+++ Megatron/bot/__init__.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 from ..vars import Var
 
 StreamBot = Client(
-    session_name="Megatron",
+  session_name=Var.SESSION_NAME,
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     workdir="Megatron",
